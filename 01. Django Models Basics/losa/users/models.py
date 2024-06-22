fdb5c13cbca1636a8a404840eee6eb3d9b61679d
Emil <e.roydev@gmail.com>
2024-06-22 03:04:09 +0300

    updates

---
+++ 01. Django Models Basics/losa/users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxLengthValidator, MinLengthValidator
+
+
+class User(models.Model):
+    class CityChoices(models.TextChoices):
+        VARNA = 'VR', 'Varna'
+        BURGAS = "BS", 'Burgas'
+        PLOVDIV = 'PL', 'Plovdiv'
+        SOFIA = 'SF', 'Sofia'
+
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
--- First_lecture/losa/script.py
+++
@@ -1,7 +0,0 @@
-import os
-import django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "losa.settings")
-django.setup()
-
-from users.models import User
