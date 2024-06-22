3cc931b864f23d473dfd99331337f5479d6b3c6e
Emil <e.roydev@gmail.com>
2024-06-22 04:19:51 +0300

    updates

--- 02. Migrations and Django Admin/Lab/main_app/models.py
+++ 02. Migrations and Django Admin/Lab/main_app/models.py
@@ -7,7 +7,10 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=100, null=True, blank=False)
     supplier = models.CharField(max_length=150, null=True, blank=False)
-    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
     last_edited_on = models.DateTimeField(auto_now=True, editable=False)
     count = models.PositiveIntegerField(default=0)
     barcode = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} with {self.price}$'
