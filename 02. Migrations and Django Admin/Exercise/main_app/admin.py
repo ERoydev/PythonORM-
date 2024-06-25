060d981150d16d4bd7d3e61670da592bb5fabfd3
Emil <e.roydev@gmail.com>
2024-06-25 16:37:07 +0300

    updates

--- 02. Migrations and Django Admin/Exercise/main_app/admin.py
+++ 02. Migrations and Django Admin/Exercise/main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import EventRegistration
+from .models import EventRegistration, Movie, Student
 
 
 @admin.register(EventRegistration)
@@ -7,3 +7,29 @@ class EventRegistrationAdmin(admin.ModelAdmin):
     list_display = ['event_name', 'participant_name', 'registration_date']
     list_filter = ['event_name', 'registration_date']
     search_fields = ['event_name', 'participant_name']
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title', 'director', 'release_year', 'genre']
+    list_filter = ['release_year', 'genre']
+    search_fields = ['title', 'director']
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'age', 'grade']
+    list_filter = ['age', 'grade', 'date_of_birth']
+    search_fields = ['first_name']
+    fieldsets = (
+        ('Personal Information',
+            {
+                'fields': ['first_name', 'last_name', 'age', 'date_of_birth']
+            }
+        ),
+        ('Academic Information',
+            {
+                'fields': ['grade']
+            }
+        )
+    )
