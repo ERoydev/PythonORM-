436ebbcf6c7d6646db56c3369f9c444078f806d7
Emil <e.roydev@gmail.com>
2024-07-17 23:11:08 +0300

    finished exercise

--- 08. Advanced Queries in Django/Lab/caller.py
+++ 08. Advanced Queries in Django/Lab/caller.py
@@ -6,110 +6,56 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models
-from main_app.models import Product, Category, Customer, Order, OrderProduct
-from django.db.models import Value, Avg, Sum, Count, CharField, Q, F
-from django.db.models.functions import Concat
-
-# # Create and run queries
-def add_records_to_database():
-    # Categories
-    food_category = Category.objects.create(name='Food')
-    drinks_category = (Category.objects.create(name='Drinks'))
-
-    # Food
-    product1 = Product.objects.create(name='Pizza', description='Delicious pizza with toppings', price=10.99, category=food_category, is_available=False)
-    product2 = Product.objects.create(name='Burger', description='Classic burger with cheese and fries', price=7.99, category=food_category, is_available=False)
-    product3 = Product.objects.create(name='Apples', description='A bag of juicy red apples', price=3.99, category=food_category, is_available=True)
-    product4 = Product.objects.create(name='Bread', description='A freshly baked loaf of bread', price=2.49, category=food_category, is_available=True)
-    product5 = Product.objects.create(name='Pasta and Sauce Bundle', description='Package containing pasta and a jar of pasta sauce', price=6.99, category=food_category, is_available=False)
-    product6 = Product.objects.create(name='Tomatoes', description='A bundle of ripe, red tomatoes', price=2.99, category=food_category, is_available=True)
-    product7 = Product.objects.create(name='Carton of Eggs', description='A carton containing a dozen fresh eggs', price=3.49, category=food_category, is_available=True)
-    product8 = Product.objects.create(name='Cheddar Cheese', description='A block of aged cheddar cheese', price=7.99, category=food_category, is_available=False)
-    product9 = Product.objects.create(name='Milk', description='A gallon of fresh cow milk', price=3.49, category=food_category, is_available=True)
-
-    # Drinks
-    product10 = Product.objects.create(name='Coca Cola', description='Refreshing cola drink', price=1.99, category=drinks_category, is_available=True)
-    product11 = Product.objects.create(name='Orange Juice', description='Freshly squeezed orange juice', price=2.49, category=drinks_category, is_available=False)
-    product12 = Product.objects.create(name='Bottled Water', description='A 12-pack of purified bottled water', price=4.99, category=drinks_category, is_available=True)
-    product13 = Product.objects.create(name='Orange Soda', description='A 6-pack of carbonated orange soda', price=5.49, category=drinks_category, is_available=True)
-    product14 = Product.objects.create(name='Bottled Green Tea', description='A bottled green tea', price=3.99, category=drinks_category, is_available=False)
-    product15 = Product.objects.create(name='Beer', description='A bottled craft beer', price=5.49, category=drinks_category, is_available=True)
-
-    # Customers
-    customer1 = Customer.objects.create(username='john_doe')
-    customer2 = Customer.objects.create(username='alex_alex')
-    customer3 = Customer.objects.create(username='peter132')
-    customer4 = Customer.objects.create(username='k.k.')
-    customer5 = Customer.objects.create(username='peter_smith')
-
-    # Orders
-    order1 = Order.objects.create(customer=customer1)
-    order_product1 = OrderProduct.objects.create(order=order1, product=product3, quantity=2)
-    order_product2 = OrderProduct.objects.create(order=order1, product=product6, quantity=1)
-    order_product3 = OrderProduct.objects.create(order=order1, product=product7, quantity=5)
-    order_product4 = OrderProduct.objects.create(order=order1, product=product13, quantity=1)
-
-    order2 = Order.objects.create(customer=customer3)
-    order_product5 = OrderProduct.objects.create(order=order2, product=product3, quantity=2)
-    order_product6 = OrderProduct.objects.create(order=order2, product=product9, quantity=1)
-
-    order3 = Order.objects.create(customer=customer1)
-    order_product5 = OrderProduct.objects.create(order=order3, product=product12, quantity=4)
-    order_product6 = OrderProduct.objects.create(order=order3, product=product7, quantity=3)
-    return "All data entered!"
-
-def product_quantity_ordered():
-    result = []
-
-    orders = Product.objects.annotate(
-        total=Sum('orderproduct__quantity')
-    ).values('name', 'total').order_by('-total').exclute(total=None)
-
-    for order in orders:
-        result.append(f"Quantity ordered of {order['name']}: {order['total']}")
-
-    return '\n'.join(result)
+from main_app.models import Rea
 
 
+RealEstateListing.objects.create(
+    property_type='House',
+    price=100000.00,
+    bedrooms=3,
+    location='Los Angeles'
+)
+RealEstateListing.objects.create(
+    property_type='Flat',
+    price=75000.00,
+    bedrooms=2,
+    location='New York City'
+)
 
-def ordered_products_per_customer():
-    orders = Order.objects.prefetch_related('products', 'customer', 'products__category')
-    result = []
+RealEstateListing.objects.create(
+    property_type='Villa',
+    price=250000.00,
+    bedrooms=4,
+    location='Los Angeles'  # Same location as the first instance
+)
 
-    for o in orders:
-        result.append(f'Order ID: {o.id}, Customer: {o.customer.username}')
-        for p in o.products.all():
-            result.append(f'- Product: {p.name}, Category: {p.category.name}')
+RealEstateListing.objects.create(
+    property_type='House',
+    price=120000.00,
+    bedrooms=3,
+    location='San Francisco'
+)
 
-    return '\n'.join(result)
+# Run the 'by_property_type' method
+house_listings = RealEstateListing.objects.by_property_type('House')
+print("House listings:")
+for listing in house_listings:
+    print(f"- {listing.property_type} in {listing.location}")
 
+# Run the 'in_price_range' method
+affordable_listings = RealEstateListing.objects.in_price_range(75000.00, 120000.00)
+print("Price in range listings:")
+for listing in affordable_listings:
+    print(f"- {listing.property_type} in {listing.location}")
 
-def filter_products():
-    products = Product.objects.filter(Q(price__gt=3) & Q(is_available=True)).order_by('-price', 'name')
-    result = []
+# Run the 'with_bedrooms' method
+two_bedroom_listings = RealEstateListing.objects.with_bedrooms(2)
+print("Two-bedroom listings:")
+for listing in two_bedroom_listings:
+    print(f"- {listing.property_type} in {listing.location}")
 
-    for p in products:
-        result.append(f'{p.name}: {p.price}lv.')
-
-    return '\n'.join(result)
-
-def give_discount():
-    query = Q(is_available=True) & Q(price__gt=3)
-    products_to_be_discounted = Product.objects.filter(query).order_by('-price', 'name')
-    products_to_be_discounted.update(
-        price=F('price') * 0.7
-    )
-
-    available_products = Product.objects.filter(is_available=True).order_by('-price', 'name')
-
-    result = []
-    for p in available_products:
-        result.append(f'{p.name}: {p.price}lv.')
-
-    return '\n'.join(result)
-
-
-products = Product.objects.raw('SELECT * FROM main_app_product')
-
-for p in products:
-    print(p)
+# Run the 'popular_locations' method
+popular_locations = RealEstateListing.objects.popular_locations()
+print("Popular locations:")
+for location in popular_locations:
+    print(f"- {location['location']} ; Listings: {location['location_count']}")
